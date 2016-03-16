ca312aec7a082a1dcb15c108b7fdd5a7a67fb33a
jemazter <jean.mazurkievicz@gmail.com>
2016-03-15 21:20:55 -0300

    Estruturas e Inicio dos Graficos
    Criada as estruturas e inicio da manipulação de graficos criando o menu
    dropdown com gtk.

--- resistores/main.c
+++ resistores/main.c
@@ -1,7 +1,68 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 
-static void helloWorld (GtkWidget *wid, GtkWidget *win)
+
+struct resistores
+{
+    float resistencia;
+    int serie;
+    float tolerancia;
+    float potencia;
+    int quantidade;
+};
+typedef struct resistores Resistores;
+
+struct serie
+{
+    struct serie *esquerda;
+    int numero_serie;
+    float valor_tolerancia;
+    struct potencia* potencia;
+    struct serie *direita;
+};
+typedef struct serie Serie;
+
+struct potencia
+{
+    struct potencia *esquerda;
+    float potencia_resistor;
+    struct resistencia* resistencia;
+    struct potencia *direita;
+};
+typedef struct potencia Potencia;
+
+struct resistencia
+{
+    struct resistencia *esquerda;
+    float valor_resistencia;
+    int quantidade_resistores;
+    struct resistencia *direita;
+};
+typedef struct resistencia Resistencia;
+
+struct header
+{
+    struct lista* iniciodeheader;
+    int nelementos;
+    struct lista* fimdeheader;
+};
+typedef struct header Estruturadoheader;
+
+struct lista
+{
+    struct lista* listaanterior;
+    struct resistores* ponteirodedados;
+    struct lista* listaprox;
+};
+typedef struct lista Lista;
+
+void inic_header(Estruturadoheader* Header)
+{
+    Header->nelementos = 0;  //inicializando
+    Header->iniciodeheader = NULL;   //inicializando
+    Header->fimdeheader = NULL;      //inicializando
+}
+/*static void helloWorld (GtkWidget *wid, GtkWidget *win)
 {
   GtkWidget *dialog = NULL;
 
@@ -9,41 +70,54 @@ static void helloWorld (GtkWidget *wid, GtkWidget *win)
   gtk_window_set_position (GTK_WINDOW (dialog), GTK_WIN_POS_CENTER);
   gtk_dialog_run (GTK_DIALOG (dialog));
   gtk_widget_destroy (dialog);
-}
+}*/
 
 int main (int argc, char *argv[])
 {
-  GtkWidget *button = NULL;
-  GtkWidget *win = NULL;
-  GtkWidget *vbox = NULL;
+  GtkWidget *window;
+  GtkWidget *vbox;
 
-  /* Initialize GTK+ */
-  g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, (GLogFunc) gtk_false, NULL);
-  gtk_init (&argc, &argv);
-  g_log_set_handler ("Gtk", G_LOG_LEVEL_WARNING, g_log_default_handler, NULL);
+  GtkWidget *menubar;
+  GtkWidget *fileMenu;
+  GtkWidget *fileMi;
+  GtkWidget *mostra_tudoMi;
+  GtkWidget *quitMi;
+  GtkWidget *pesquisaMi;
 
-  /* Create the main window */
-  win = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_container_set_border_width (GTK_CONTAINER (win), 8);
-  gtk_window_set_title (GTK_WINDOW (win), "Hello World");
-  gtk_window_set_position (GTK_WINDOW (win), GTK_WIN_POS_CENTER);
-  gtk_widget_realize (win);
-  g_signal_connect (win, "destroy", gtk_main_quit, NULL);
+  gtk_init(&argc, &argv);
 
-  /* Create a vertical box with buttons */
-  vbox = gtk_vbox_new (TRUE, 6);
-  gtk_container_add (GTK_CONTAINER (win), vbox);
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+  gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+  gtk_window_set_title(GTK_WINDOW(window), "Simple menu");
 
-  button = gtk_button_new_from_stock (GTK_STOCK_DIALOG_INFO);
-  g_signal_connect (G_OBJECT (button), "clicked", G_CALLBACK (helloWorld), (gpointer) win);
-  gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
+  vbox = gtk_vbox_new(FALSE, 0);
+  gtk_container_add(GTK_CONTAINER(window), vbox);
 
-  button = gtk_button_new_from_stock (GTK_STOCK_CLOSE);
-  g_signal_connect (button, "clicked", gtk_main_quit, NULL);
-  gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, TRUE, 0);
+  menubar = gtk_menu_bar_new();
+  fileMenu = gtk_menu_new();
 
-  /* Enter the main loop */
-  gtk_widget_show_all (win);
-  gtk_main ();
+  fileMi = gtk_menu_item_new_with_label("Arquivo");
+  mostra_tudoMi = gtk_menu_item_new_with_label("Mostrar Estoque");
+  pesquisaMi = gtk_menu_item_new_with_label("Pesquisar Estoque");
+  quitMi = gtk_menu_item_new_with_label("Sair");
+
+  gtk_menu_item_set_submenu(GTK_MENU_ITEM(fileMi), fileMenu);
+  gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), mostra_tudoMi);
+  gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), pesquisaMi);
+  gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu), quitMi);
+  gtk_menu_shell_append(GTK_MENU_SHELL(menubar), fileMi);
+  gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 0);
+
+  g_signal_connect(G_OBJECT(window), "destroy",
+        G_CALLBACK(gtk_main_quit), NULL);
+
+  g_signal_connect(G_OBJECT(quitMi), "activate",
+        G_CALLBACK(gtk_main_quit), NULL);
+
+  gtk_widget_show_all(window);
+
+  gtk_main();
+
   return 0;
 }
